13be4734c8168552697761992fbe5bcef8b94562
shubhanshu Tiwari <149867878+shubhanshut31@users.noreply.github.com>
2025-09-16 10:31:54 +0530

    Implement accordion functionality for FAQ items

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-const tabItems = document.querySelectorAll(".tab-item");
-const tabContentItems = document.querySelectorAll(".tab-content-item");
+let accordian = document.getElementsByClassName("FAQ__title");
 
-// Select tab content
-function selectItem(e) {
-  removeBorder();
-  removeShow();
-  // Add border to current tab
-  this.classList.add("tab-border");
-  // Grab content item from DOM
-  const tabContentItem = document.querySelector(`#${this.id}-content`);
-  // Add show class
-  tabContentItem.classList.add("show");
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
-
-function removeBorder() {
-  tabItems.forEach(item => item.classList.remove("tab-border"));
-}
-
-function removeShow() {
-  tabContentItems.forEach(item => item.classList.remove("show"));
-}
-
-// Listen for tab click
-tabItems.forEach(item => item.addEventListener("click", selectItem));
